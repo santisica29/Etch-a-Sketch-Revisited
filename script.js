288dfd153edebc44fbd17de76882c1fc620c2e0c
Santiago Sica <santisica29@gmail.com>
2025-08-12 22:37:53 -0300

    figure out how to erase a grid's color

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 makeGrid(16);
-console.log('hola')
-function makeGrid(size){
-    let gridSize = size * size;
+console.log("hola");
+function makeGrid(size) {
+  let gridSize = size * size;
 
-    for (let i=0; i < gridSize; i++){
-        
-        let grid = document.createElement('div');
+  for (let i = 0; i < gridSize; i++) {
+    let grid = document.createElement("div");
 
-        grid.classList.add('grid');
+    grid.classList.add("grid");
 
-        grid.addEventListener('mouseover', (e) => {
-            
-            let doesItHaveAColorSet = e.currentTarget.style.backgroundColor = 'white';
-            if (doesItHaveAColorSet){
-                
-            }
-            e.currentTarget.style.backgroundColor = 'red';
-        })
+    grid.addEventListener("mouseover", (e) => {
+      let doesItHaveAColorSet = e.currentTarget.style.backgroundColor != "";
+      if (doesItHaveAColorSet) {
+        e.currentTarget.style.backgroundColor = "";
+      } else {
+        e.currentTarget.style.backgroundColor = "red";
+      }
+    });
 
-        container.appendChild(grid);
-    }
+    container.appendChild(grid);
+  }
 }
